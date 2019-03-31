87fd014828966b9830cab69ba0632309d02f264b
Aman <amsingh714@gmail.com>
2019-03-31 11:49:50 -0400

    able to do toggle completed items

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component, useState } from "react";
 import "./App.css";
 
-import TodoList from "./components/TodoList"
+import TodoList from "./components/TodoList";
 
 const todosData = [
   {
-    task: 'Organize Garage',
+    task: "Organize Garage",
     id: 1528817077286,
     completed: false
   },
   {
-    task: 'Bake Cookies',
+    task: "Bake Cookies",
     id: 1528817084358,
     completed: false
   }
@@ -51,7 +51,7 @@ export class App extends Component {
 export default function HooksApp() {
   const [title, setTitle] = useState("I am a title using hooks.");
   const todoInput = useInput();
-  const [todos, setTodos] = useState(todosData)
+  const [todos, setTodos] = useState(todosData);
 
   // const updateTitle = e => {
   //   e.preventDefault();
@@ -65,15 +65,26 @@ export default function HooksApp() {
       task: todoInput.value,
       id: Date.now(),
       completed: false
-    }
-    setTodos([...todos, newTodo ])
-    todoInput.setValue("")
-  }
+    };
+    setTodos([...todos, newTodo]);
+    todoInput.setValue("");
+  };
+
+  const toggleTodo = id => {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      })
+    );
+  };
 
   return (
     <div className="App">
       <h1>{title}</h1>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} handleToggleComplete={toggleTodo} />
       <form onSubmit={addTodo}>
         <input
           type="text"
@@ -95,6 +106,9 @@ const useInput = () => {
 
   return { value, updateValue, setValue };
 };
+
+
+
 
 /* 
 const obj = { oneProp: 'one', twoProp: 'two' };
